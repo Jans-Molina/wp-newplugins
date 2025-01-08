5e715f558da36c5d10db36ab3e4cb53b9613f1a2
Jans-Molina <jvns.molina1@gmail.com>
2025-01-08 13:00:18 -0300

    Se agrego el primer plugins para desarrollar holamundo.php

---
+++ 01-holamundo/holamundo.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Mi Primer Plugin
+Description: Este es un plugin básico para demostrar cómo se crea un plugin en WordPress.
+Version: 1.0
+Author: Jans Molina
+*/
+
+// Tu código PHP aquí
